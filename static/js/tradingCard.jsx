98e880c16093b0bbf5c3048737c7cafc7e834dd9
vinitha <vinitha.nec@gmail.com>
2023-04-22 17:07:34 -0700

    Complete Lab1

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -2,11 +2,57 @@
 
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
+  },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
   },
 ];
+
+// console.log(tradingCardData); 
+
 
 function TradingCard(props) {
   return (
@@ -18,29 +64,48 @@ function TradingCard(props) {
   );
 }
 
-ReactDOM.render(
-  <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
-  document.querySelector('#balloonicorn'),
-);
+function TradingCardContainer() {
+  const tradingCards = []; 
 
-ReactDOM.render(
-  <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
-  document.querySelector('#float'),
-);
+  for (const currentCard of tradingCardData) {
+    tradingCards.push(
+      // <p>currentCard.name</p>
+      <TradingCard 
+        name={currentCard.name} 
+        skill={currentCard.skill} 
+        imgUrl={currentCard.imgUrl} 
+      /> 
+    ); 
+  }
 
-ReactDOM.render(
-  <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
-  document.querySelector('#llambda'),
-);
-
-ReactDOM.render(
-  <TradingCard name="polymorphism" skill="eating icecream" imgUrl="/static/img/polymorphism.jpeg" />,
-  document.querySelector('#polymorphism'),
-);
+  return (<React.Fragment>{tradingCards}</React.Fragment>); 
+}
+ReactDOM.render(<TradingCardContainer />, document.querySelector('#all-cards'));
 
 
-ReactDOM.render(
-  <TradingCard name="seedpy" skill="riding the bi-cycle" imgUrl="/static/img/seedpy.jpeg" />,
-  document.querySelector('#seedpy'),
-);
+// ReactDOM.render(
+//   <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
+//   document.querySelector('#balloonicorn'),
+// );
+
+// ReactDOM.render(
+//   <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
+//   document.querySelector('#float'),
+// );
+
+// ReactDOM.render(
+//   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
+//   document.querySelector('#llambda'),
+// );
+
+// ReactDOM.render(
+//   <TradingCard name="polymorphism" skill="eating icecream" imgUrl="/static/img/polymorphism.jpeg" />,
+//   document.querySelector('#polymorphism'),
+// );
+
+
+// ReactDOM.render(
+//   <TradingCard name="seedpy" skill="riding the bi-cycle" imgUrl="/static/img/seedpy.jpeg" />,
+//   document.querySelector('#seedpy'),
+// );
 
